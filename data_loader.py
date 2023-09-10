d3ed289de7a2802931c2c2bb01f1f899f28bd8ed
aramiracle <a.r.amouzad.m@gmail.com>
2023-09-10 13:51:06 +0330

    add colorjitter

--- data_loader.py
+++ data_loader.py
@@ -5,46 +5,52 @@ import os
 import random
 import torch
 
+
 class ImageEnhancementDataset(Dataset):
     def __init__(self, input_root_dir, output_root_dir, transform=None, train=True):
         self.input_root_dir = input_root_dir
         self.output_root_dir = output_root_dir
         self.transform = transform
-        self.input_image_list = os.listdir(input_root_dir)
-        self.output_image_list = os.listdir(output_root_dir)
         self.train = train
 
+        # List all input and output image file paths
+        self.input_image_paths = [os.path.join(input_root_dir, fname) for fname in os.listdir(input_root_dir)]
+        self.output_image_paths = [os.path.join(output_root_dir, fname) for fname in os.listdir(output_root_dir)]
+
     def __len__(self):
-        return len(self.input_image_list)
+        return len(self.input_image_paths)
 
-    def add_gaussian_blur(self, image, kernel_size=5, sigma=0.5):
-        if random.random() < 0.5:  # Apply blur with 50% probability
-            image = transforms.GaussianBlur(kernel_size, sigma)(image)
-        return image
-
-    def add_gaussian_noise(self, image, mean=0, std=0.001):
-        if random.random() < 0.5:  # Apply noise with 50% probability
-            image = transforms.ToTensor()(image)
-            noise = torch.randn_like(image) * std + mean
-            image = image + noise
-            image = transforms.ToPILImage()(image)  # Convert back to PIL Image
-        return image
-
-    def __getitem__(self, idx):
-        input_img_name = os.path.join(self.input_root_dir, self.input_image_list[idx])
-        output_img_name = os.path.join(self.output_root_dir, self.output_image_list[idx])
-
-        input_image = Image.open(input_img_name).convert('RGB')
-        output_image = Image.open(output_img_name).convert('RGB')
-
+    def apply_data_augmentation(self, input_image, output_image):
         if self.train:
-            # Apply random rotation to both input and output images
-            angle = random.randint(-45, 45)  # You can adjust the rotation range as needed
+            # Random rotation
+            angle = random.randint(-30, 30)
             input_image = input_image.rotate(angle)
             output_image = output_image.rotate(angle)
 
-            input_image = self.add_gaussian_blur(input_image)
-            input_image = self.add_gaussian_noise(input_image)
+            # Color Jitter
+            color_jitter = transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)
+            input_image = color_jitter(input_image)
+            output_image = color_jitter(output_image)
+
+            # Gaussian blur and noise
+            if random.random() < 0.5:
+                input_image = transforms.GaussianBlur(5, 0.5)(input_image)
+            if random.random() < 0.5:
+                input_image = transforms.ToTensor()(input_image)
+                noise = torch.randn_like(input_image) * 0.001 + 0
+                input_image = input_image + noise
+                input_image = transforms.ToPILImage()(input_image)
+
+        return input_image, output_image
+
+    def __getitem__(self, idx):
+        input_img_path = self.input_image_paths[idx]
+        output_img_path = self.output_image_paths[idx]
+
+        input_image = Image.open(input_img_path).convert('RGB')
+        output_image = Image.open(output_img_path).convert('RGB')
+
+        input_image, output_image = self.apply_data_augmentation(input_image, output_image)
 
         if self.transform:
             input_image = self.transform(input_image)
